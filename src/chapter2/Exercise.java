71e4728b6477087a4bd0da7ba21e11af4d6708a3
Mihail Damyanov <m.damyanov.d@gmail.com>
2024-01-05 20:43:31 +0200

    Adding code for chapters 3a and 3b.

--- src/chapter2/Exercise.java
+++ src/chapter2/Exercise.java
@@ -2,6 +2,12 @@ package chapter2;
 
 import java.util.Scanner;
 
+/*
+Create a program that asks for a season of the year,
+then a whole number, then an adjective.
+Use the input to complete the sentence below then output the result:
+"On a [adjective][season of the year] day, I drink a minimum of [whole number] cups of coffee."
+ */
 public class Exercise {
     public static void main(String[] args) {
         System.out.println("Enter season of the year:");
