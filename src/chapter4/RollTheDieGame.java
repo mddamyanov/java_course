13ca3b6faf431fe2c04d3a8b0f41fdcc12f72179
Mihail Damyanov <m.damyanov.d@gmail.com>
2024-01-07 17:13:39 +0200

    Adding code for chapters 4c and 4d. Adding code for exercise over chapter 4.

---
+++ src/chapter4/RollTheDieGame.java
@@ -0,0 +1,41 @@
+package chapter4;
+
+import java.util.Random;
+
+/*
+The objective of this game is to travel the entire game board of 20 spaces within 5 die rolls.
+ */
+public class RollTheDieGame {
+    public static void main(String[] args) {
+
+        //Initialize what we know
+        int maxRolls = 5;
+        int currentSpace = 0;
+        int lastSpace = 20;
+        int spacesLeft;
+        Random random = new Random();
+
+        //Roll the dice
+        for (int i = 1; i <= maxRolls; i++) {
+
+            int die = random.nextInt(6) + 1;
+            currentSpace = currentSpace + die;
+
+            System.out.println("Roll #" + i + ": You've rolled a " + die + ".");
+
+            if (currentSpace == lastSpace) {
+                System.out.println("Congrats! You won.");
+                break;
+            } else if (currentSpace > lastSpace) {
+                System.out.println("Unfortunately, that takes you past " + lastSpace + " spaces. You lose!");
+                break;
+            } else if (i == maxRolls && currentSpace < lastSpace) {
+                System.out.println("You didn't get to " + lastSpace + " spaces. You lose!");
+            } else {
+                spacesLeft = lastSpace - currentSpace;
+                System.out.println("You are now on space " + currentSpace + " and have " + spacesLeft + " more to go.");
+            }
+            System.out.println();
+        }
+    }
+}
